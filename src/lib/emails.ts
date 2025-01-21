6301ab5519bc3873da659ffa643a001b38821c4f
harshau007 <amanupadhyay2004@gmail.com>
2025-01-21 18:57:40 +0530

    Added others email

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -5,14 +5,15 @@ interface emailType {
 
 export const emails: emailType[] = [
   { username: "Harsh-U", email: "upadhyayharsh533@gmail.com" },
-  { username: "dhanashri_kuwar", email: "" },
-  { username: "Yagyesh21", email: "" },
-  { username: "LaGatito", email: "" },
-  { username: "amxn-sh", email: "" },
-  { username: "_aryaaann_", email: "" },
-  { username: "_anchall02", email: "" },
+  { username: "dhanashri_kuwar", email: "dhanashrikuwar25@gmail.com" },
+  { username: "Yagyesh21", email: "yagyeshyadav2005@gmail.com" },
+  { username: "LaGatito", email: "amir.kamal09@gmail.com" },
+  { username: "amxn-sh", email: "amansharmaa1205@gmail.com" },
+  { username: "_aryaaann_", email: "aryanshukla9004@gmail.com" },
+  { username: "_anchall02", email: "anchalvtiwari@gmail.com" },
   { username: "Harshsahani10", email: "" },
-  { username: "shreyam462", email: "" },
+  { username: "shreyam462", email: "shreya.m462@gmail.com" },
   { username: "swarnimmungad", email: "" },
-  { username: "Lakshya_2005", email: "" },
+  { username: "Lakshya_2005", email: "beyondlakshya@gmail.com" },
+  { username: "dhirajkalwar57", email: "dhirajkalwar57@gmail.com" },
 ];
